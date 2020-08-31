a128d029a3a64fb0637a8e95e96e6061a2c70683
Trevor <trevorgschwartz@gmail.com>
2020-08-31 02:51:33 -0400

    generating different rooms with different timers basics working

--- client/components/socket.tsx
+++ client/components/socket.tsx
@@ -1,11 +1,14 @@
-import React, { FunctionComponent, useState, useEffect } from 'react'
+import React, { FunctionComponent, useState, useEffect, ChangeEvent, FormEvent } from 'react'
 import socketIOClient from "socket.io-client"
+
 
 const ENDPOINT = "http://127.0.0.1:4001";
 let socket: SocketIOClient.Socket
 
 const Socket:FunctionComponent = () => {
     const [response, setResponse] = useState('')
+    const [roomNumber, setRoomNumber] = useState(0)
+    const [generatedRoomNumber, setGeneratedRoomNumber] = useState(NaN)
 
     useEffect(() => {
          socket = socketIOClient(ENDPOINT);
@@ -14,7 +17,6 @@ const Socket:FunctionComponent = () => {
             });
 
         return () => {socket.disconnect()}
-        
       }, []);
 
        const startTimer = () => {
@@ -22,12 +24,38 @@ const Socket:FunctionComponent = () => {
         socket.emit('start-Timer')
       }
 
+    const generateRoom = () => {
+        let randomRoomNumber = Math.floor(Math.random() * 100000)
+        setGeneratedRoomNumber(randomRoomNumber)
+        socket.emit('enter-Room', randomRoomNumber)
+    }
+
+    const handleEnterRoomChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setRoomNumber(Number(e.target.value))
+    }
+    
+    const enterRoom = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        socket.emit('enter-Room', roomNumber)
+    }
+
     return (
         <>
         <p>
             Time Left {response}
         </p>
         <button onClick={() => {startTimer()}}>Start Timer</button>
+        <button onClick={generateRoom}>Generate Room</button>
+        <form onSubmit={enterRoom}>
+            <label>
+                Enter Room #
+                <input type="text" name="name" value={roomNumber} onChange={(e: ChangeEvent<HTMLInputElement>):void => handleEnterRoomChange(e)}/>
+            </label>
+        <input type="submit" value="Submit" />
+        </form>
+        {isNaN(generatedRoomNumber) ? null : 
+        <p>Your Room Number is {generatedRoomNumber}</p>
+        }
         </>
     )
 }
