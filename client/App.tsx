440a71d1936c6f691c660665ec68393ebb95cad0
Trevor <trevorgschwartz@gmail.com>
2020-09-02 16:44:40 -0400

    a few tiny changes

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,4 @@
-
-import * as React from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from './index'
 import { addToHelloWorld } from './actions/index'
--- client/index.tsx
+++ client/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, applyMiddleware } from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducers/index'
 import { AppActions } from './types/actions'
+import rootReducer from './reducers/index'
+import { Provider } from 'react-redux';
+import ReactDOM from "react-dom";
+import React from 'react';
 
 import App from './App';
 import "./styles.scss";
+
+
 
 export type AppState = ReturnType<typeof rootReducer>
 
@@ -16,10 +18,12 @@ const initialState = { helloWorld: "Hello World" }
 
 export const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)))
 
-const mountNode = document.getElementById("app");
+
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    mountNode
+    document.getElementById("app")
 )
+    
