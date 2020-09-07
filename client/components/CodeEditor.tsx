12d5d98a536e2d03d333a7817e8a6538daa4a770
Julia McNeill <juliammcneill@gmail.com>
2020-09-07 16:28:11 -0400

    Edit light mode theme colors

--- client/components/CodeEditor.tsx
+++ client/components/CodeEditor.tsx
@@ -1,37 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { AppState } from '../types';
-import { ControlledEditor } from '@monaco-editor/react';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { AppState } from "../types";
+import { ControlledEditor } from "@monaco-editor/react";
 
 interface Props {
   socketSendCodeUpdate: (val: any) => void;
 }
 
 export const CodeEditor: React.FC<Props> = ({ socketSendCodeUpdate }) => {
-  const codeEditorData: any = useSelector((state: AppState) => state.codeEditorData);
-  const [dimensions, setDimensions] = useState<{ [key: string]: any }>({ width: 500, height: 300 });
-  const [value, setValue] = useState('// Write code here');
-  const [theme, setTheme] = useState('dark');
-  const [language, setLanguage] = useState('javascript');
+  const codeEditorData: any = useSelector(
+    (state: AppState) => state.codeEditorData
+  );
+  const [dimensions, setDimensions] = useState<{ [key: string]: any }>({
+    width: 500,
+    height: 300,
+  });
+  const [value, setValue] = useState("// Write code here");
+  const [theme, setTheme] = useState("light");
+  const [language, setLanguage] = useState("javascript");
   const [isEditorReady, setIsEditorReady] = useState(false);
   const languages = [
-    'cpp',
-    'csharp',
-    'csp',
-    'go',
-    'java',
-    'javascript',
-    'kotlin',
-    'php',
-    'python',
-    'ruby',
-    'rust',
-    'swift',
-    'typescript',
+    "cpp",
+    "csharp",
+    "csp",
+    "go",
+    "java",
+    "javascript",
+    "kotlin",
+    "php",
+    "python",
+    "ruby",
+    "rust",
+    "swift",
+    "typescript",
   ];
 
   const handleEditorDidMount = () => setIsEditorReady(true);
-  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
   const selectLanguage = (e: any) => {
     setLanguage(e.target.value);
     socketSendCodeUpdate({ value, language: e.target.value });
@@ -43,29 +48,35 @@ export const CodeEditor: React.FC<Props> = ({ socketSendCodeUpdate }) => {
 
   useEffect(() => {
     /* @ts-ignore  */
-    const height = document.getElementsByClassName('CodeEditor')[0].clientHeight;
+    const height = document.getElementsByClassName("CodeEditor")[0]
+      .clientHeight;
     /* @ts-ignore  */
-    const width = document.getElementsByClassName('CodeEditor')[0].clientWidth;
+    const width = document.getElementsByClassName("CodeEditor")[0].clientWidth;
     setDimensions({ width, height });
   }, [isEditorReady]);
 
   useEffect(() => {
-    if (codeEditorData.language !== language) setLanguage(codeEditorData.language);
+    if (codeEditorData.language !== language)
+      setLanguage(codeEditorData.language);
     setValue(codeEditorData.value);
   }, [codeEditorData]);
 
   return (
     // https://www.npmjs.com/package/@monaco-editor/react for details on props
-    <div className='CodeEditorContainer'>
+    <div className="CodeEditorContainer">
       <button onClick={toggleTheme} disabled={!isEditorReady}>
         {theme} mode
       </button>
-      <select disabled={!isEditorReady} value={language} onChange={selectLanguage}>
+      <select
+        disabled={!isEditorReady}
+        value={language}
+        onChange={selectLanguage}
+      >
         {languages.map((lng) => (
           <option value={`${lng}`}>{lng}</option>
         ))}
       </select>
-      <div className='CodeEditor'>
+      <div className="CodeEditor">
         <ControlledEditor
           width={dimensions.width}
           height={dimensions.height}
