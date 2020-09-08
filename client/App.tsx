796afa8f8ac744e4a7ccd53987fadc55b0b348bd
Michael Doudy <michael.doudy@gmail.com>
2020-09-08 11:19:49 -0400

    Add data file which keeps track of all active rooms and respective data

--- client/App.tsx
+++ client/App.tsx
@@ -16,21 +16,20 @@ export const App: React.FC = () => {
   const [isTimerOn, toggleIsTimerOn] = useState<boolean>(false);
   const [timer, setTimer] = useState<number>(0);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | number | null>(null);
-  const socketSendMessage = (text: string) => socket.emit('send-chat-message', room, text);
+  const socketSendMessage = (text: string) => socket.emit('send-text-update', room, text);
   const socketSendCanvasUpdate = (text: string) => socket.emit('send-canvas-update', room, text);
   const socketSendCodeUpdate = (data: any) => socket.emit('send-code-update', room, data);
   const socketSendPromptUpdate = (prompt: any) => socket.emit('send-prompt-update', room, prompt);
   const socketToggleTimer = (bool: boolean) => socket.emit('send-timer-update', room, bool);
 
   useEffect((): any => {
-    socket.on('chat-message', (data: any) => dispatch(setGlobalText(data)));
+    socket.on('text-update', (data: any) => dispatch(setGlobalText(data)));
     socket.on('canvas-update', (data: any) => dispatch(setGlobalContextData(data)));
     socket.on('code-update', (data: any) => dispatch(setCodeEditorData(data)));
     socket.on('prompt-update', (prompt: any) => dispatch(setPromptData(prompt)));
     socket.on('timer-update', (bool: boolean) => toggleIsTimerOn(bool));
-    socket.on('user-joined-room', (data: any) => {
-      console.log(data);
-    });
+    socket.on('user-joined-room', (data: any) => { console.log(data) });
+    socket.on('user-left-room', (data: any) => { console.log(data) });
     return socket.disconnect;
   }, []);
 
